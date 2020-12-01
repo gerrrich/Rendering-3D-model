45e147fe810c241be0cc034d304025864b8d474a
gerrrich <gerrich@my.com>
2020-12-01 17:28:56 +0300

    all without light

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.image as img
+import xxsubtype
 
 
 def draw_line(x0, y0, x1, y1, image, color, location_x, location_y, view_height):
@@ -83,19 +84,20 @@ def m_view_port(x=0, y=0, w=1024, h=1024):
 
 
 def norm(v):
-    no = 0
-    for i in v:
-        no += i ** 2
+    no = v[0] ** 2 + v[1] ** 2 + v[2] ** 2
     no = np.sqrt(no)
     if no == 0:
         return v
-    return v / no
+    if len(v) == 4:
+        return np.array([v[0] / no, v[1] / no, v[2] / no, v[3]])
+    else:
+        return np.array([v[0] / no, v[1] / no, v[2] / no])
 
 
 def back_face_culling(p, v0, v1, v2):
     v1 = np.array([v1[0] - v0[0], v1[1] - v0[1], v1[2] - v0[2]])
     v2 = np.array([v2[0] - v0[0], v2[1] - v0[1], v2[2] - v0[2]])
-    v0 = np.array([v0[0] - p[0], v0[1] - p[1], v0[2] - p[2]])
+    # v0 = np.array([v0[0] - p[0], v0[1] - p[1], v0[2] - p[2]])
     nn = norm(np.cross(v1, v2))
     s = np.inner(np.array([0, 0, -1]), nn)
     # s = np.inner(v0, nn)
@@ -109,11 +111,29 @@ def get_barycentric_coordinates(p, v0, v1, v2):
     bb1 = v1[1] - v0[1]
     cc0 = v2[0] - v0[0]
     cc1 = v2[1] - v0[1]
-
     c = (aa0 * bb1 - aa1 * bb0) / (cc0 * bb1 - cc1 * bb0)
     b = (aa1 - c * cc1) / bb1
     a = 1 - b - c
     return a, b, c
+
+
+def cos_vec(L1, N1):
+    L = norm(L1)
+    N = norm(N1)
+
+    return L[0] * N[0] + L[1] * N[1] + L[2] * N[2]
+
+
+def lighting(ka, ia, kd, id, ks, Is, N, L, V, alp, a, b, c, d):
+    # (1, 25, 0.8, 180, 0.8, 255, norm(N), sun, camera, 10, 1, 0.02, 0.004, d)
+    la = ka * ia  # 25
+    ld = kd * id * cos_vec(L, N)  # 0-150
+    r = 2 * cos_vec(L, N) * N / norm(N) - L
+    ls = ks * Is * (cos_vec(r, V)) ** alp  # 0-205
+    ans = la + (ld + ls)  # 25-380
+    ans2 = a + b * d + c * d ** 2  # 1+0.02*d+0.004*d
+    # print(cos_vec(L, N))
+    return la + (ld + ls) / (a + b * d + c * d ** 2)  # должен быть от 0 до 115 примерно
 
 
 class LocalToWorld:
@@ -149,12 +169,11 @@ class WorldToCamera:
                       [0, 1, 0, -self.camera_location_vec[1]],
                       [0, 0, 1, -self.camera_location_vec[2]],
                       [0, 0, 0, 1]])
-
         return L.dot(R)
 
 
 class Model:
-    def __init__(self, obj_path: str, texture_path: str):
+    def __init__(self, obj_path: str, texture_path: str, sun: np.ndarray):
         self.obj_path = obj_path
         self.texture_path = texture_path
 
@@ -165,7 +184,11 @@ class Model:
         self.vertices = []
         self.new_vertices = []
         self.texture_v = []
+        self.normals = []
+        self.new_normals = []
         self.faces = []
+        self.sun = sun
+        self.camera = 0
 
         for line in obj:
             temp = line[:-1].split()
@@ -175,19 +198,24 @@ class Model:
                 self.vertices.append(np.array([float(temp[1]), float(temp[2]), float(temp[3]), 1]))
             elif temp[0] == 'vt':
                 self.texture_v.append([float(temp[1]), float(temp[2])])
+            elif temp[0] == 'vn':
+                self.normals.append(np.array([float(temp[1]), float(temp[2]), float(temp[3]), 1]))
             elif temp[0] == 'f':
                 cur1 = temp[1].split('/')
                 cur2 = temp[2].split('/')
                 cur3 = temp[3].split('/')
-                self.faces.append([[int(cur1[0]) - 1, int(cur1[1]) - 1],
-                                   [int(cur2[0]) - 1, int(cur2[1]) - 1],
-                                   [int(cur3[0]) - 1, int(cur3[1]) - 1]])
+                self.faces.append([[int(cur1[0]) - 1, int(cur1[1]) - 1, int(cur1[2]) - 1],
+                                   [int(cur2[0]) - 1, int(cur2[1]) - 1, int(cur2[2]) - 1],
+                                   [int(cur3[0]) - 1, int(cur3[1]) - 1, int(cur3[2]) - 1]])
 
     def create_camera_coordinates(self, l2w: LocalToWorld, w2c: WorldToCamera):
         m = w2c.get_matrix().dot(l2w.get_matrix())
+        mn = np.linalg.inv(m).T
+        self.sun = m.dot(self.sun)
+        self.camera = w2c.camera_location_vec
         for i in range(len(self.vertices)):
             self.new_vertices.append(m.dot(self.vertices[i]))
-        return self.new_vertices
+            self.new_normals.append(mn.dot(self.normals[i]))
 
 
 def get_wire_image(view_height: int, view_width: int, image_height: int, image_width: int,
@@ -365,4 +393,100 @@ def get_texture_image(view_height: int, view_width: int, image_height: int, imag
 
 def get_texture_image_with_with_light(view_height: int, view_width: int, image_height: int, image_width: int,
                                       location_image_x: int, location_image_y: int, model: Model):
-    pass
+    image = np.zeros((view_height, view_width, 3), dtype=np.uint8)
+    z_buffer = np.ones((view_height, view_width), dtype=np.float)
+    image[:] = np.array([150, 150, 150])
+
+    for j in range(location_image_y, location_image_y + image_height):
+        for i in range(location_image_x, location_image_x + image_width):
+            image[view_height - j - 1][i] = np.array([0, 0, 0], dtype=np.uint8)
+
+    vertices = model.new_vertices
+    texture_img = model.texture_img
+    texture_v = model.texture_v
+    normals = model.new_normals
+    faces = model.faces
+    sun = model.sun
+    camera = np.array([model.camera[0], model.camera[1], model.camera[2], 1])
+
+    ll = min(vertices, key=lambda x: x[0])
+    rr = max(vertices, key=lambda x: x[0])
+    bb = min(vertices, key=lambda x: x[1])
+    tt = max(vertices, key=lambda x: x[1])
+    nn = min(vertices, key=lambda x: x[2])
+    ff = max(vertices, key=lambda x: x[2])
+
+    l = ll[0] - (rr[0] - ll[0]) / 20
+    r = rr[0] + (rr[0] - ll[0]) / 20
+    b = bb[1] - (tt[1] - bb[1]) / 20
+    t = tt[1] + (tt[1] - bb[1]) / 20
+    n = nn[2] - (ff[2] - nn[2]) / 20
+    f = ff[2] + (ff[2] - nn[2]) / 20
+
+    m = m_view_port(location_image_x, location_image_y, image_width, image_height).dot(m_proj_o(l, r, b, t, n, f))
+    mn = np.linalg.inv(m).T
+
+    camera = m.dot(camera)
+    new_sun = m.dot(sun)
+
+    for face in faces:
+        vv1 = vertices[face[0][0]]
+        vv2 = vertices[face[1][0]]
+        vv3 = vertices[face[2][0]]
+
+        vn1 = normals[face[0][2]]
+        vn2 = normals[face[1][2]]
+        vn3 = normals[face[2][2]]
+
+        if back_face_culling([0, 0, 0], vv1, vv2, vv3) >= 0:
+            continue
+
+        v1 = m.dot(vv1)
+        v2 = m.dot(vv2)
+        v3 = m.dot(vv3)
+
+        vn1 = mn.dot(vn1)
+        vn2 = mn.dot(vn2)
+        vn3 = mn.dot(vn3)
+
+        x_min = int(np.floor(min(v1, v2, v3, key=lambda x: x[0])[0]))
+        x_max = int(np.ceil(max(v1, v2, v3, key=lambda x: x[0])[0]))
+        y_min = int(np.floor(min(v1, v2, v3, key=lambda x: x[1])[1]))
+        y_max = int(np.ceil(max(v1, v2, v3, key=lambda x: x[1])[1]))
+
+        for i in range(x_min, x_max + 1):
+            for j in range(y_min, y_max + 1):
+                a, b, c = get_barycentric_coordinates([i, j], v1, v2, v3)
+                # an, bn, cn = get_barycentric_coordinates([i, j], vv1, vv2, vv3)
+
+                N = vn1 * a + vn2 * b + vn3 * c
+
+                p = a * vv1 + b * vv2 + c * vv3
+
+                d = np.sqrt((p[0] - sun[0]) ** 2 + (p[1] - sun[1]) ** 2 + (p[2] - sun[2]) ** 2)
+                # print(d)
+
+                light1 = lighting(1, 25, 0.8, 180, 0.8, 255, norm(N), new_sun, camera, 10, 1, 0.02, 0.004, d)
+                light2 = lighting(1, 25, 1, 150, 0.8, 255, norm(N), new_sun, camera, 10, 1, 0.02, 0.004, d)
+                light3 = lighting(1, 25, 0.9, 190, 0.8, 255, norm(N), new_sun, camera, 10, 1, 0.02, 0.004, d)
+                # print(light1, light2, light3, d)
+
+                if a >= 0 and b >= 0 and c >= 0:
+                    z = a * v1[2] + b * v2[2] + c * v3[2]
+                    if -z < z_buffer[view_height - j, i]:
+                        z_buffer[view_height - j, i] = -z
+                        u = a * texture_v[face[0][1]][0] + b * texture_v[face[1][1]][0] + c * texture_v[face[2][1]][0]
+                        v = a * texture_v[face[0][1]][1] + b * texture_v[face[1][1]][1] + c * texture_v[face[2][1]][1]
+
+                        color = texture_img[len(texture_img) - round(v * len(texture_img))][
+                            round(u * len(texture_img[0]))]
+                        color1 = round(color[0] * light1 / 255)
+                        color1 = 255 if color1 > 255 else color1
+                        color2 = round(color[1] * light2 / 255)
+                        color2 = 255 if color2 > 255 else color2
+                        color3 = round(color[2] * light3 / 255)
+                        color3 = 255 if color3 > 255 else color3
+                        color = np.array([color1, color2, color3])
+
+                        image[view_height - j][i] = color
+    return image
--- main.py
+++ main.py
@@ -4,26 +4,27 @@ import matplotlib.pyplot as plt
 
 l2w = LocalToWorld(5, 10, 15, [-1, 0, -2], [0.8, 0.8, 0.8])
 w2c = WorldToCamera([2, 2, 2], [-2, -2, 0])
-model = Model('obj/african_head/african_head.obj', 'obj/african_head/african_head_diffuse.tga')
+
+model = Model('obj/african_head/african_head.obj', 'obj/african_head/african_head_diffuse.tga', np.array([4, 4, 3, 1]))
 model.create_camera_coordinates(l2w, w2c)
 
-# image1 = get_wire_image(1024, 1024, 500, 500, 8, 16 + 500, model)
-# image2 = get_face_image(1024, 1024, 500, 500, 16 + 500, 16 + 500, model)
-# image3 = get_texture_image(1024, 1024, 500, 500, 8, 8, model)
+image1 = get_wire_image(1024, 1024, 500, 500, 8, 16 + 500, model)
+image2 = get_face_image(1024, 1024, 500, 500, 16 + 500, 16 + 500, model)
+image3 = get_texture_image(1024, 1024, 500, 500, 8, 8, model)
 image4 = get_texture_image_with_with_light(1024, 1024, 500, 500, 16 + 500, 8, model)
 image = np.zeros((1024, 1024, 3), dtype=np.uint8)
 
-# for i in range(1024):
-#     for j in range(1024):
-#         if i < 512 and j < 512:
-#             image[j][i] = image1[j][i]
-#         elif i >= 512 and j < 512:
-#             image[j][i] = image2[j][i]
-#         elif i < 512 and j >= 512:
-#             image[j][i] = image3[j][i]
-#         elif i >= 512 and j >= 512:
-#             image[j][i] = image4[j][i]
+for i in range(1024):
+    for j in range(1024):
+        if i < 512 and j < 512:
+            image[j][i] = image1[j][i]
+        elif i >= 512 and j < 512:
+            image[j][i] = image2[j][i]
+        elif i < 512 and j >= 512:
+            image[j][i] = image3[j][i]
+        elif i >= 512 and j >= 512:
+            image[j][i] = image4[j][i]
 
 fig = plt.figure(figsize=(10, 10))
-plt.imshow(image4)
+plt.imshow(image)
 plt.show()
